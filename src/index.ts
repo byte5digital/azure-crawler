9a780a678d20885d6de4038774324af00c0b4e95
Marcel Wege <mwege@byte5.de>
2020-05-14 14:35:10 +0200

    Initial Release

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import * as express from "express"
+
+export const app = express()
+
+const whois = require("whois")
+const domainPing = require("domain-ping")
+
+app.get("/", async (req, res) => {
+	res.statusCode = 404
+	res.send()
+})
+
+app.get("/:domain", async (req, res) => {
+	console.log("new Request")
+	res.statusCode = 200
+	domainPing(req.params.domain).then(async (data) => {
+	  console.log(data)
+		
+		await whois.lookup(data.ip, (errW, dataW) => {
+			  console.log(errW)
+			  console.log(dataW)
+			res.statusCode = 200
+			res.json({
+				ip: data.ip,
+				isAzure: dataW.includes("msndcc@microsoft.com")
+			})
+		})
+		
+	})
+		.catch((error)=>{
+			console.log(error)
+			res.statusCode = 500
+			res.send()
+		})
+
+
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+})
