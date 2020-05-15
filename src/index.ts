cc2051cf16617ebb9b351f8f7836bb1f05c0de58
Marcel Wege <mwege@byte5.de>
2020-05-15 14:44:43 +0200

    Bug Fix

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,8 @@ app.get("/", async (req, res) => {
 
 app.get("/:domain", async (req, res) => {
   await nslookup(req.params.domain).end(async (err, data) => {
-    if (data && data.length > 0) {
+	  if (data && data.length > 0) {
+		console.log(data[0])
       await whois.lookup(data[0], async (errW, dataW) => {
         return res.json({
           ip: data[0],
