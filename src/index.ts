db10c49d5b81589f76a37b5f0c1c8e892dbc8c2c
Marcel Wege <mwege@byte5.de>
2020-05-15 15:04:38 +0200

    BugFix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as express from "express"
 import { readSync } from "fs"
+import { json } from "body-parser"
 
 export const app = express()
 
-const whois = require("whois")
+const whois = require("whois-json")
 const nslookup = require("nslookup")
 
 app.get("/", async (req, res) => {
@@ -15,17 +16,14 @@ app.get("/:domain", async (req, res) => {
   await nslookup(req.params.domain).end(async (err, data) => {
     if (data && data.length > 0) {
       console.log(data[0])
-      await whois.lookup(data[0], async (errW, dataW) => {
-        if (errW) {
-          return res.status(404).send()
-        }
-        return res.json({
-          ip: data[0],
-          isAzure: dataW.includes("msndcc@microsoft.com"),
-        })
+      let whoisResult = await whois(data[0])
+      return res.json({
+        ip: data[0],
+        isAzure: whoisResult.orgName.includes("Microsoft"),
+        registrar: whoisResult.orgName,
       })
-	} else {
-		console.log('not found')
+    } else {
+      console.log("not found")
       return res.status(404).send()
     }
   })
