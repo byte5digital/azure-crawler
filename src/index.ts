576609e10b4773c95657b6b333c9facd37c8bd7d
Marcel Wege <mwege@byte5.de>
2020-05-15 14:47:39 +0200

    Bug Fix

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,20 @@ app.get("/", async (req, res) => {
 
 app.get("/:domain", async (req, res) => {
   await nslookup(req.params.domain).end(async (err, data) => {
-	  if (data && data.length > 0) {
-		console.log(data[0])
+    if (data && data.length > 0) {
+      console.log(data[0])
       await whois.lookup(data[0], async (errW, dataW) => {
+        if (errW) {
+          return res.status(404).send()
+        }
         return res.json({
           ip: data[0],
           isAzure: dataW.includes("msndcc@microsoft.com"),
         })
       })
-      if (err) {
-        return res.status(404).send()
-      }
+	} else {
+		console.log('not found')
+      return res.status(404).send()
     }
   })
 })
